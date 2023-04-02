d3c05c504b43f258b637ce2c81af8410516cd819
Rafael <rafael.tlv@proton.me>
2023-04-02 12:56:48 +0300

    added crontab

--- run_tests.sh
+++ run_tests.sh
@@ -3,3 +3,5 @@
 source /Users/Apikorus/QAProjects/AutomationTests/env/bin/activate
 
 pytest -n 5 --html=report.html
+
+(crontab -l ; echo "0 0 * * * /bin/bash /path/to/your/script.sh") | crontab -
--- tests/test_main.py
+++ tests/test_main.py
@@ -10,6 +10,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+import configparser
 
 
 class TestWebsite():
@@ -83,6 +84,7 @@ class TestWebsite():
             signinicon.click()
             popupwindow = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > div.bEJky.w._Z.t._U.s.l.D.Za")))
             assert popupwindow.get_attribute('outerHTML') in driver.page_source
+            print
         except (TimeoutException, AssertionError) as e:
             pytest.fail(f"An error occurred during test execution: {e}")
 
