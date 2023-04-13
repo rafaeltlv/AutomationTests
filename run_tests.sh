48a3fc95d45e170334ba678f97fd7e952b89f934
Rafael <rafael.tlv@proton.me>
2023-04-13 12:09:02 +0300

    sh file update

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Check if the cron job already exists
+if ! crontab -l | grep -q "/bin/bash /Users/Apikorus/QAProjects/AutomationTests/run_tests.sh"; then
+    # Add the cron job
+    (crontab -l 2>/dev/null; echo "0 0 * * * /bin/bash /Users/Apikorus/QAProjects/AutomationTests/run_tests.sh") | crontab -
+fi
+
+# Activate the virtual environment
 source /Users/Apikorus/QAProjects/AutomationTests/env/bin/activate
 
+# Run the tests
 pytest -n 5 --html=report.html
 
-(crontab -l ; echo "0 0 * * * /bin/bash /path/to/your/script.sh") | crontab -
+exit 0
