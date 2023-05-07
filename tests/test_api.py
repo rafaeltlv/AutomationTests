7fe3fcd8ed6ceef3e93ba29bedfa6141f905a193
Rafael <rafael.tlv@proton.me>
2023-05-07 19:01:30 +0300

    rename of files for clarity

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,17 +4,14 @@ import httpretty
 from ..resources import context
 from ..resources.conftest import driver_init
 
+
 @httpretty.activate
 def test_registration_api(driver_init):
-    httpretty.register_uri(httpretty.GET, "https://www.tripadvisor.co.il/RegistrationController", 
+    httpretty.register_uri(httpretty.GET, "https://www.tripadvisor.co.il/", 
                         body="Access Denied")
     response = requests.get('https://www.tripadvisor.co.il/RegistrationController')
     assert response.text == "Access Denied"
     httpretty.disable()
     httpretty.reset()
-
-
-
-
 
 #add more apis
